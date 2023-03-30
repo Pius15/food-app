cf7bc5a05438cc6e630586f6634fe3c06863555c
pius15 <dadsonm34@gmail.com>
2023-03-30 19:18:45 +0300

    Learn initialize

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Food from './components/Food';
+
+
+function App() {
+  return (
+    <div>
+      <h1>lets get started</h1>
+      <Food />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Food.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Food() {
+  return (
+    <div>
+        <h2>food menu</h2>
+        <p>our restaurant will be up soon</p>
+        <p>we are open now</p>
+        <p>come all let us enjoy</p>
+    </div>
+  )
+}
+
+export default Food
